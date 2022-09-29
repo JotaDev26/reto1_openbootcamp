c4296f42fef83dd390f50fa8ffc1ec3a4518f333
JotaElDev <jotadev25@gmail.com>
2022-09-28 21:26:25 -0500

    last commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// const saludo =
+//   "Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo";
+console.log(
+  "Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo"
+);
